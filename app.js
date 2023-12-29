c297f5fb41a2c8e262aeb8635846cbb38f4bdcdb
nufc009 <imran_nufc@hotmail.co.uk>
2023-12-29 19:58:04 +0000

    Edit index.html and app.js

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(){
     
-    createBoard(16)
-    
+    createBoard(16);
+
+    let btn_popup = document.querySelector("#popup")
+    btn_popup.addEventListener("click", function(){
+        let size = getSize();
+        createBoard(size);
+    })
 })
 
 function createBoard(size){
@@ -17,6 +22,21 @@ function createBoard(size){
          div.style.backgroundColor = "orange";
          board.insertAdjacentElement("beforeend", div);
     
+    }
+
+}
+
+function getSize(){
+    let input = prompt("Choose the size of the Board?");
+    let message = document.querySelector("#message")
+
+    if(input == ""){
+        message.innerHTML = "Please provide a number";
+    } else if(input < 0 || input > 100){
+        message.innerHTML = "Please provide a number between 1 and 100";
+    } else {
+        message.innerHTML = "Now you can draw";
+        return input;
     }
 
 }
